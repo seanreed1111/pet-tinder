48d51bb54b14badf0924465c78da02bd0b5d6f64
soccerkevin <soccerkevint11@gmail.com>
2019-07-22 16:47:06 -0700

    switch to a deck

--- src/components/search.js
+++ src/components/search.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
-import { Carousel } from 'react-responsive-carousel';
+import Swipeable from "react-swipy"
 
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "stylesheets/search.scss";
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { pets: props.pets }
+  }
+
+  remove(dir) {
+    let pet = this.state.pets.shift()
+    this.setState({ pets: this.state.pets })
+  }
+
   render() {
     return (
-      <Carousel
-        showThumbs={false}
-        showStatus={false}
-        showIndicators={false}
-        showArrows={false}
-        className="search pane"
-      >
+      <div className="search pane">
         {
-          this.props.pets.map( (pet) => {
+          this.state.pets.map( (pet, i) => {
             return (
-              <div className="pet" key={pet.id}>
-                <figure>
-                  <img alt={`${pet.type}: ${pet.name}`} src={pet.img} />
-                </figure>
-                <h3 className="name">{`${pet.name}, ${pet.age}yr, ${pet.sex}`}</h3>
-                <div className="description">{pet.profile}</div>
+              <div
+                style={{zIndex: 100 - i}}
+                className="pet"
+                key={pet.id}
+              >
+                <Swipeable onSwipe={this.remove.bind(this)} >
+                    <figure>
+                      <img alt={`${pet.type}: ${pet.name}`} src={pet.img} />
+                    </figure>
+                    <h3 className="name">{`${pet.name}, ${pet.age}yr, ${pet.sex}`}</h3>
+                    <div className="description">{pet.profile}</div>
+                </Swipeable>
               </div>
             )
           })
         }
-      </Carousel>
+      </div>
     )
   }
 }
