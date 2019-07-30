bf8eb888f92ad92035b15e53d1fbfdb6e9877fb2
soccerkevin <soccerkevint11@gmail.com>
2019-07-30 16:36:30 -0700

    test deck buttons

--- src/tests/deck.test.js
+++ src/tests/deck.test.js
@@ -15,10 +15,30 @@ describe("Deck", () => {
     );
   })
 
+  afterEach(() => {
+    store.clearActions()
+  })
+
   it('renders slides', () => {
     expect(deck.find('.pet').length).toBe(2);
   })
 
+  describe("actions", () => {
+    it('accepts pets on click', () => {
+      deck.find('.control.right').simulate('click');
+      const save = store.getActions()[0];
+      const remove = store.getActions()[1];
+      expect(save.type).toBe("SAVE_PET");
+      expect(save.pet.id).toBe(1001);
+      expect(remove.type).toBe("REMOVE_PET");
+    });
+
+    it('rejects pets on click', () => {
+      deck.find('.control.left').simulate('click');
+      const remove = store.getActions()[0];
+      expect(remove.type).toBe("REMOVE_PET");
+    });
+  })
 })
 
 describe('Deck', () => {
